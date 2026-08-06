8f7ab4b84615efb2a1c9ed1573967d9636ff9ce4
Dean Henny Anton Lemans Haar <deanlemans@mailbox.org>
2026-08-06 02:09:47 +0200

    audio component doesn cross reference anymore

--- scripts/components/animation_component.gd
+++ scripts/components/animation_component.gd
@@ -2,11 +2,14 @@
 class_name AnimationComponent
 extends Node
 
+signal step(is_active: bool)
+
 @export var buffer_duration: float = 0.3
 @export var idle_delay: float = 3.0
 
 var walk_buffer: float = 0.0
 var idle_time: float = 0.0
+var is_actively_moving: bool = false
 var current_horizontal: MovementComponent.Horizontal = MovementComponent.DEFAULT_HORIZONTAL
 var current_vertical: MovementComponent.Vertical = MovementComponent.DEFAULT_VERTICAL
 
@@ -16,6 +19,7 @@ var current_vertical: MovementComponent.Vertical = MovementComponent.DEFAULT_VER
 func _ready() -> void:
 	assert(sprite, "no Sprite.")
 	sprite.play("start")
+	sprite.frame_changed.connect(_on_frame_changed)
 
 
 func play_walk(
@@ -24,12 +28,14 @@ func play_walk(
 ) -> void:
 	walk_buffer = buffer_duration
 	idle_time = 0.0
+	is_actively_moving = true
 	current_horizontal = horizontal
 	current_vertical = vertical
 	_play_if_needed(_walk_anim_name())
 
 
 func update_walk_buffer(delta: float) -> void:
+	is_actively_moving = false
 	walk_buffer -= delta
 	if walk_buffer > 0.0:
 		_play_if_needed(_walk_anim_name())
@@ -41,6 +47,7 @@ func update_walk_buffer(delta: float) -> void:
 
 
 func play_idle() -> void:
+	is_actively_moving = false
 	_play_if_needed("idle")
 
 
@@ -53,3 +60,12 @@ func _walk_anim_name() -> String:
 func _play_if_needed(anim_name: String) -> void:
 	if sprite.animation != anim_name or not sprite.is_playing():
 		sprite.play(anim_name)
+
+
+func _on_frame_changed() -> void:
+	if _is_walk_animation():
+		step.emit(is_actively_moving)
+
+
+func _is_walk_animation() -> bool:
+	return sprite.animation != "idle" and sprite.animation != "start"
--- scripts/components/audio_component.gd
+++ scripts/components/audio_component.gd
@@ -1,5 +1,6 @@
 # audio_component.gd
 ### actual dogshit code i need to rewire to my current system
+### okey, i rewrote it
 class_name AudioComponent
 extends Component
 
@@ -8,16 +9,16 @@ extends Component
 @export var footstep_cooldown: float = 0.2
 @export var footstep_buffer_duration: float = 0.1
 
-var animation: AnimationComponent
-var movement: MovementComponent
 var _footstep_timer: float = 0.0
 var _footstep_buffer: float = 0.0
 
 @onready var player: AudioStreamPlayer2D = %AudioStreamPlayer2D
+@onready var animation: AnimationComponent = get_sibling(AnimationComponent)
 
 
 func _ready() -> void:
-	call_deferred("_connect_siblings")
+	if animation:
+		animation.step.connect(_on_step)
 
 
 func _process(delta: float) -> void:
@@ -43,23 +44,8 @@ func play_fire() -> void:
 	_play_one_shot(fire_sound)
 
 
-func _connect_siblings() -> void:
-	animation = get_sibling(AnimationComponent)
-	movement = get_sibling(MovementComponent)
-	if not animation or not movement:
-		push_error("missing sibling")
-		return
-	animation.sprite.frame_changed.connect(_on_frame_changed)
-
-
-func _on_frame_changed() -> void:
-	if not movement.move_state.get("active"):
-		return
-	var state = movement.move._last_state
-	if (
-		state == GUIDEAction.GUIDEActionState.TRIGGERED
-		or state == GUIDEAction.GUIDEActionState.ONGOING
-	):
+func _on_step(is_active: bool) -> void:
+	if is_active:
 		_footstep_buffer = footstep_buffer_duration
 		play_footstep()
 	elif _footstep_buffer > 0.0:
