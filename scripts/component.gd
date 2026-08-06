476937b7e3a416190c7b52c453bc956f3466f98c
Dean Henny Anton Lemans Haar <deanlemans@mailbox.org>
2026-08-06 23:06:10 +0200

    reqrite so that its more flexible

--- scripts/component.gd
+++ scripts/component.gd
@@ -4,7 +4,7 @@ extends Node
 
 
 # find a sibling by type, not just Components
-static func find_sibling_of_type(from: Node, type: Script, warn_if_missing: bool = true) -> Node:
+static func find_sibling_of_type(from: Node, type: Variant, warn_if_missing: bool = true) -> Node:
 	var parent: Node = from.get_parent()
 	if not parent:
 		push_error("%s: no parent." % from.name)
@@ -13,20 +13,20 @@ static func find_sibling_of_type(from: Node, type: Script, warn_if_missing: bool
 		if is_instance_of(child, type):
 			return child
 	if warn_if_missing:
-		push_warning("%s: no sibling of type %s." % [from.name, type.resource_path])
+		push_warning("%s: no sibling of type %s." % [from.name, type])
 	return null
 
 
 # same but for children, jumppad need it, probb should update that
-static func find_child_of_type(from: Node, type: Script, warn_if_missing: bool = false) -> Node:
+static func find_child_of_type(from: Node, type: Variant, warn_if_missing: bool = false) -> Node:
 	for child: Node in from.get_children():
 		if is_instance_of(child, type):
 			return child
 	if warn_if_missing:
-		push_warning("%s: no child of type %s." % [from.name, type.resource_path])
+		push_warning("%s: no child of type %s." % [from.name, type])
 	return null
 
 
 # components can get_sibling(AnimationComponent)
-func get_sibling(type: Script, warn_if_missing: bool = true) -> Node:
+func get_sibling(type: Variant, warn_if_missing: bool = true) -> Node:
 	return Component.find_sibling_of_type(self, type, warn_if_missing)
