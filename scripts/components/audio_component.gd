dca144e08c76172cfaaec159e6c22bd50c33994c
Dean Henny Anton Lemans Haar <deanlemans@mailbox.org>
2026-08-08 01:55:50 +0200

    made audio separate from animation

--- scripts/components/audio_component.gd
+++ scripts/components/audio_component.gd
@@ -1,4 +1,3 @@
-# audio_component.gd
 ### actual dogshit code i need to rewire to my current system
 ### okey, i rewrote it
 class_name AudioComponent
@@ -6,11 +5,10 @@ extends Component
 
 @export var footstep_sounds: Array[AudioStream] = []
 @export var fire_sound: AudioStream
-@export var footstep_cooldown: float = 0.2
-@export var footstep_buffer_duration: float = 0.1
+@export var footstep_interval: float = 0.35
 
+var _is_walking: bool = false
 var _footstep_timer: float = 0.0
-var _footstep_buffer: float = 0.0
 
 @onready var player: AudioStreamPlayer2D = %AudioStreamPlayer2D
 @onready var animation: AnimationComponent = get_sibling(AnimationComponent)
@@ -22,20 +20,17 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	if _footstep_timer > 0.0:
-		_footstep_timer -= delta
-	if _footstep_buffer > 0.0:
-		_footstep_buffer -= delta
+	if not _is_walking:
+		return
+	_footstep_timer -= delta
+	if _footstep_timer <= 0.0:
+		_footstep_timer = footstep_interval
+		play_footstep()
 
 
 func play_footstep() -> void:
 	if footstep_sounds.is_empty():
 		return
-	if _footstep_timer > 0.0:
-		return
-	if player.playing:
-		return
-	_footstep_timer = footstep_cooldown
 	player.stream = footstep_sounds.pick_random()
 	player.play()
 
@@ -45,11 +40,8 @@ func play_fire() -> void:
 
 
 func _on_step(is_active: bool) -> void:
-	if is_active:
-		_footstep_buffer = footstep_buffer_duration
-		play_footstep()
-	elif _footstep_buffer > 0.0:
-		play_footstep()
+	_is_walking = is_active
+	_footstep_timer = 0.0
 
 
 func _play_one_shot(stream: AudioStream) -> void:
