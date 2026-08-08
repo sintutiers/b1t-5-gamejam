7cf70558f012bd700d945ed1e01feb7050214515
Dean Henny Anton Lemans Haar <deanlemans@mailbox.org>
2026-08-08 23:36:48 +0200

    updated theme, added dialogue

--- scripts/components/dialogue_component.gd
+++ scripts/components/dialogue_component.gd
@@ -9,21 +9,25 @@ var _movement: MovementComponent
 
 @onready var interactable: Interactable = get_parent() as Interactable
 
-
 func _ready() -> void:
+	if not interactable:
+		interactable = Component.find_sibling_of_type(self, Interactable, false) as Interactable
+		if interactable:
+			push_warning(
+				"%s: Interactable is sibling. Prefer child of Interactable."
+				% name,
+			)
 	assert(interactable, "Interactable needed.")
 	if not dialogue_resource:
 		push_warning("No DialogueResource.")
 	interactable.interacted.connect(_on_interacted)
 	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 
-
 func _exit_tree() -> void:
 	if interactable and interactable.interacted.is_connected(_on_interacted):
 		interactable.interacted.disconnect(_on_interacted)
 	if DialogueManager.dialogue_ended.is_connected(_on_dialogue_ended):
 		DialogueManager.dialogue_ended.disconnect(_on_dialogue_ended)
-
 
 func _on_interacted(by: RapierArea2D) -> void:
 	if not dialogue_resource:
@@ -32,7 +36,6 @@ func _on_interacted(by: RapierArea2D) -> void:
 	if _movement:
 		_movement.is_interacting = true
 	DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start)
-
 
 func _on_dialogue_ended(_resource: DialogueResource) -> void:
 	if _movement:
