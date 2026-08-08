93724d24d5433543aed867ba179c3cdda47795f6
Dean Henny Anton Lemans Haar <deanlemans@mailbox.org>
2026-08-09 00:35:39 +0200

    added audio to dialogue talking
    its also configurable

--- assets/dialogue/balloon.gd
+++ assets/dialogue/balloon.gd
@@ -1,6 +1,11 @@
 extends CanvasLayer
 ## A basic dialogue balloon for use with Dialogue Manager.
 
+## The action to use for advancing the dialogue
+const NEXT_ACTION = &"ui_accept"
+
+## The action to use to skip typing the dialogue
+const SKIP_ACTION = &"ui_cancel"
 
 ## The dialogue resource
 @export var dialogue_resource: DialogueResource
@@ -14,17 +19,11 @@ extends CanvasLayer
 ## If all other input is blocked as long as dialogue is shown.
 @export var will_block_other_input: bool = true
 
-## The action to use for advancing the dialogue
-@export var next_action: StringName = &"ui_accept"
-
-## The action to use to skip typing the dialogue
-@export var skip_action: StringName = &"ui_cancel"
-
-## A sound player for voice lines (if they exist).
-@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
-
 ## Temporary game states
 var temporary_game_states: Array = []
+
+## The DialogueComponent for the current speaker, if any, for per-letter talk sounds
+var speaker: DialogueComponent
 
 ## See if we are waiting for the player
 var is_waiting_for_input: bool = false
@@ -33,9 +32,7 @@ var is_waiting_for_input: bool = false
 var will_hide_balloon: bool = false
 
 ## A dictionary to store any ephemeral variables
-var locals: Dictionary = {}
-
-var _locale: String = TranslationServer.get_locale()
+var locals: Dictionary = { }
 
 ## The current line
 var dialogue_line: DialogueLine:
@@ -54,6 +51,10 @@ var dialogue_line: DialogueLine:
 
 ## A cooldown timer for delaying the balloon hide when encountering a mutation.
 var mutation_cooldown: Timer = Timer.new()
+var _locale: String = TranslationServer.get_locale()
+
+## A sound player for voice lines (if they exist).
+@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
 
 ## The base balloon anchor
 @onready var balloon: Control = %Balloon
@@ -74,23 +75,30 @@ var mutation_cooldown: Timer = Timer.new()
 func _ready() -> void:
 	balloon.hide()
 	Engine.get_singleton("DialogueManager").mutated.connect(_on_mutated)
+	dialogue_label.spoke.connect(_on_dialogue_label_spoke)
 
 	# If the responses menu doesn't have a next action set, use this one
 	if responses_menu.next_action.is_empty():
-		responses_menu.next_action = next_action
+		responses_menu.next_action = NEXT_ACTION
 
 	mutation_cooldown.timeout.connect(_on_mutation_cooldown_timeout)
 	add_child(mutation_cooldown)
 
 	if auto_start:
 		if not is_instance_valid(dialogue_resource):
-			assert(false, DMConstants.get_error_message(DMConstants.ERR_MISSING_RESOURCE_FOR_AUTOSTART))
+			assert(
+				false,
+				DMConstants.get_error_message(DMConstants.ERR_MISSING_RESOURCE_FOR_AUTOSTART),
+			)
 		start()
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if is_instance_valid(dialogue_line):
-		progress.visible = not dialogue_label.is_typing and dialogue_line.responses.size() == 0 and not dialogue_line.has_tag("voice")
+		progress.visible = (
+			not dialogue_label.is_typing and dialogue_line.responses.size() == 0
+			and not dialogue_line.has_tag("voice")
+		)
 
 
 func _unhandled_input(_event: InputEvent) -> void:
@@ -101,7 +109,10 @@ func _unhandled_input(_event: InputEvent) -> void:
 
 func _notification(what: int) -> void:
 	## Detect a change of locale and update the current dialogue line to show the new language
-	if what == NOTIFICATION_TRANSLATION_CHANGED and _locale != TranslationServer.get_locale() and is_instance_valid(dialogue_label):
+	if (
+		what == NOTIFICATION_TRANSLATION_CHANGED
+		and _locale != TranslationServer.get_locale() and is_instance_valid(dialogue_label)
+	):
 		_locale = TranslationServer.get_locale()
 		var visible_ratio: float = dialogue_label.visible_ratio
 		dialogue_line = await dialogue_resource.get_next_dialogue_line(dialogue_line.id)
@@ -110,14 +121,28 @@ func _notification(what: int) -> void:
 
 
 ## Start some dialogue
-func start(with_dialogue_resource: DialogueResource = null, title: String = "", extra_game_states: Array = []) -> void:
+func start(
+	with_dialogue_resource: DialogueResource = null,
+	title: String = "",
+	extra_game_states: Array = [],
+) -> void:
 	temporary_game_states = [self] + extra_game_states
+	speaker = extra_game_states.filter(
+		func(s):
+			return s is DialogueComponent,
+	).front() if extra_game_states.any(
+		func(s):
+			return s is DialogueComponent,
+	) else null
 	is_waiting_for_input = false
 	if is_instance_valid(with_dialogue_resource):
 		dialogue_resource = with_dialogue_resource
 	if not title.is_empty():
 		start_from_title = title
-	dialogue_line = await dialogue_resource.get_next_dialogue_line(start_from_title, temporary_game_states)
+	dialogue_line = await dialogue_resource.get_next_dialogue_line(
+		start_from_title,
+		temporary_game_states,
+	)
 	show()
 
 
@@ -130,8 +155,8 @@ func apply_dialogue_line() -> void:
 	balloon.focus_mode = Control.FOCUS_ALL
 	balloon.grab_focus()
 
-	#character_label.visible = not dialogue_line.character.is_empty()
-	#character_label.text = tr(dialogue_line.character, "dialogue")
+	character_label.visible = not dialogue_line.character.is_empty()
+	character_label.text = tr(dialogue_line.character)
 
 	dialogue_label.hide()
 	dialogue_label.dialogue_line = dialogue_line
@@ -139,7 +164,6 @@ func apply_dialogue_line() -> void:
 	responses_menu.hide()
 	responses_menu.responses = dialogue_line.responses
 
-	# Show our balloon
 	balloon.show()
 	will_hide_balloon = false
 
@@ -158,7 +182,13 @@ func apply_dialogue_line() -> void:
 		balloon.focus_mode = Control.FOCUS_NONE
 		responses_menu.show()
 	elif dialogue_line.time != "":
-		var time: float = dialogue_line.text.length() * 0.02 if dialogue_line.time == "auto" else dialogue_line.time.to_float()
+		var time: float = (
+			dialogue_line.text.length() * 0.02
+			if dialogue_line.time == "auto"
+			else dialogue_line \
+					.time \
+					.to_float()
+		)
 		await get_tree().create_timer(time).timeout
 		next(dialogue_line.next_id)
 	else:
@@ -173,8 +203,6 @@ func next(next_id: String) -> void:
 
 
 #region Signals
-
-
 func _on_mutation_cooldown_timeout() -> void:
 	if will_hide_balloon:
 		will_hide_balloon = false
@@ -191,22 +219,30 @@ func _on_mutated(mutation: Dictionary) -> void:
 func _on_balloon_gui_input(event: InputEvent) -> void:
 	# See if we need to skip typing of the dialogue
 	if dialogue_label.is_typing:
-		var mouse_was_clicked: bool = event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()
-		var skip_button_was_pressed: bool = event.is_action_pressed(skip_action)
+		var mouse_was_clicked: bool = (
+			event is InputEventMouseButton
+			and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()
+		)
+		var skip_button_was_pressed: bool = event.is_action_pressed(SKIP_ACTION)
 		if mouse_was_clicked or skip_button_was_pressed:
 			get_viewport().set_input_as_handled()
 			dialogue_label.skip_typing()
 			return
 
-	if not is_waiting_for_input: return
-	if dialogue_line.responses.size() > 0: return
+	if not is_waiting_for_input:
+		return
+	if dialogue_line.responses.size() > 0:
+		return
 
 	# When there are no response options the balloon itself is the clickable thing
 	get_viewport().set_input_as_handled()
 
-	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+	if (
+		event is InputEventMouseButton and event.is_pressed()
+		and event.button_index == MOUSE_BUTTON_LEFT
+	):
 		next(dialogue_line.next_id)
-	elif event.is_action_pressed(next_action) and get_viewport().gui_get_focus_owner() == balloon:
+	elif event.is_action_pressed(NEXT_ACTION) and get_viewport().gui_get_focus_owner() == balloon:
 		next(dialogue_line.next_id)
 
 
@@ -214,4 +250,8 @@ func _on_responses_menu_response_selected(response: DialogueResponse) -> void:
 	next(response.next_id)
 
 
+func _on_dialogue_label_spoke(letter: String, letter_index: int, _speed: float) -> void:
+	if not speaker:
+		return
+	speaker.play_talk_letter(letter, letter_index)
 #endregion
--- scripts/components/dialogue_component.gd
+++ scripts/components/dialogue_component.gd
@@ -4,24 +4,25 @@ extends Node
 
 @export var dialogue_resource: DialogueResource
 @export var dialogue_start: String = "start"
+@export var talk_sound: AudioStream
 
 var _movement: MovementComponent
 
 @onready var interactable: Interactable = get_parent() as Interactable
+@onready var player: AudioStreamPlayer2D = %AudioStreamPlayer2D
+
 
 func _ready() -> void:
 	if not interactable:
 		interactable = Component.find_sibling_of_type(self, Interactable, false) as Interactable
 		if interactable:
-			push_warning(
-				"%s: Interactable is sibling. Prefer child of Interactable."
-				% name,
-			)
+			push_warning("%s: Interactable is sibling. Prefer child of Interactable." % name)
 	assert(interactable, "Interactable needed.")
 	if not dialogue_resource:
 		push_warning("No DialogueResource.")
 	interactable.interacted.connect(_on_interacted)
 	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+
 
 func _exit_tree() -> void:
 	if interactable and interactable.interacted.is_connected(_on_interacted):
@@ -29,13 +30,27 @@ func _exit_tree() -> void:
 	if DialogueManager.dialogue_ended.is_connected(_on_dialogue_ended):
 		DialogueManager.dialogue_ended.disconnect(_on_dialogue_ended)
 
+
+func play_talk_letter(letter: String, letter_index: int) -> void:
+	if [" ", "."].has(letter):
+		return
+	if letter_index % 2 == 0:
+		return
+	if not talk_sound:
+		return
+	player.stream = talk_sound
+	player.pitch_scale = randf_range(0.8, 1.2)
+	player.play()
+
+
 func _on_interacted(by: RapierArea2D) -> void:
 	if not dialogue_resource:
 		return
 	_movement = Component.find_sibling_of_type(by, MovementComponent) as MovementComponent
 	if _movement:
 		_movement.is_interacting = true
-	DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start)
+	DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start, [self])
+
 
 func _on_dialogue_ended(_resource: DialogueResource) -> void:
 	if _movement:
