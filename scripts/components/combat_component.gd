79b99b49ce87eb52f0bb0c68ae03511014232157
Dean Henny Anton Lemans Haar <deanlemans@mailbox.org>
2026-08-10 22:51:23 +0200

    update mouse follow

--- scripts/components/combat_component.gd
+++ scripts/components/combat_component.gd
@@ -6,54 +6,57 @@ extends Component
 @export var look_absolute: GUIDEAction
 @export var fire: GUIDEAction
 @export var bolt: PackedScene
-@export var max_turn_degrees: float = 90.0
-
 @export var turn_speed: float = 5.0
+@export var max_turn_degrees: float = 80.0
+@export var side_switch_margin: float = 6.0
 
-var _facing_angle: float = 0.0
-var _initialized: bool = false
+var aim_angle: float = 0.0
+var aim_side: float = 1.0
+var started: bool = false
 
 @onready var body: Node2D = get_parent() as Node2D
 @onready var center_hand: Node2D = %CenterHand
+@onready var sprite: Node2D = %AnimatedSprite2D
 
 
 func _ready() -> void:
-	if body != null:
-		_facing_angle = body.global_rotation
-	_initialized = true
+	if body:
+		aim_angle = body.global_rotation
+	started = true
 	fire.triggered.connect(_fire)
 
 
 func _physics_process(delta: float) -> void:
 	var target: Vector2 = Vector2.INF
-
 	if look_absolute.is_triggered():
-		var canvas_pos: Vector2 = look_absolute.value_axis_2d
-		target = get_viewport().get_canvas_transform().affine_inverse() * canvas_pos
+		var mouse_pos: Vector2 = look_absolute.value_axis_2d
+		target = get_viewport().get_canvas_transform().affine_inverse() * mouse_pos
 	elif look_relative.is_triggered():
 		target = body.global_position + look_relative.value_axis_2d
-
 	if not target.is_finite():
 		return
-
-	var to_target: Vector2 = target - body.global_position
-	if to_target.length_squared() <= 0.000001:
+	var dir: Vector2 = target - center_hand.global_position
+	if dir.length_squared() <= 0.000001:
 		return
-
-	var desired_angle: float = to_target.angle()
-
-	if not _initialized:
-		_facing_angle = body.global_rotation
-		_initialized = true
-
-	var signed_delta: float = wrapf(desired_angle - _facing_angle, -PI, PI)
-	var max_delta: float = deg_to_rad(max_turn_degrees)
-	signed_delta = clamp(signed_delta, -max_delta, max_delta)
-
-	var clamped_angle: float = _facing_angle + signed_delta
-	_facing_angle = lerp_angle(_facing_angle, clamped_angle, turn_speed * delta)
-
-	body.global_rotation = _facing_angle
+	if aim_side >= 0.0:
+		if dir.x < -side_switch_margin:
+			aim_side = -1.0
+	else:
+		if dir.x > side_switch_margin:
+			aim_side = 1.0
+	if not started:
+		aim_angle = body.global_rotation
+		started = true
+	var flat_dir: Vector2 = Vector2(absf(dir.x), dir.y)
+	var base_angle: float = clamp(
+		flat_dir.angle(),
+		-deg_to_rad(max_turn_degrees),
+		deg_to_rad(max_turn_degrees),
+	)
+	var target_angle: float = base_angle * aim_side
+	aim_angle = lerp_angle(aim_angle, target_angle, turn_speed * delta)
+	body.global_rotation = aim_angle
+	sprite.scale.x = aim_side * absf(sprite.scale.x)
 
 
 func _fire() -> void:
